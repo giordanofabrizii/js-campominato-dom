bd91a2014f8015b8e517c283da0fd8a5e6aca5cf
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-20 11:45:37 +0200

    changed method to create the grid

--- js/script.js
+++ js/script.js
@@ -2,30 +2,39 @@ const campoContainerEl = document.getElementById("campo-container");
 
 const playButton = document.getElementById("start-game");
 playButton.addEventListener('click', function(){
-    
+    // prima svuoto il container
+    campoContainerEl.innerHTML = ''
+
+    // genero la griglia
+    generaGriglia();
+});
+
+function generaGriglia() {
+
     // definisco difficolta
     let difficultyChoiceEl = document.getElementById("difficulty-choice")
     let difficulty = difficultyChoiceEl.value;
 
-    //  definisco il numero di celle
-    let numberOfCell;
-    if (difficulty === "easy") {
-        numberOfCell = 49;
-    } else if(difficulty === "normal") {
-        numberOfCell = 81;
-    } else {
-        numberOfCell = 100;
+    // definisco il numero di celle
+    switch (difficulty) {
+        case '1':
+            var numberOfCell = 81;
+            var className = 'normal';
+            break;
+        case '2':
+            var numberOfCell = 49;
+            var className = 'hard';
+            break;
+        default:    
+            var numberOfCell = 100;
+            var className = 'easy';
     }
 
-
-    // prima svuoto il container
-    campoContainerEl.innerHTML = ''
-
-    // genero le celle al click del play
+    // genero n caselle
     for (let i = 1; i < numberOfCell + 1; i++) {
         let article = document.createElement("article");
         article.innerHTML = `<p>${i}</p>`
-        article.classList.add("cella" , `${difficulty}`);
+        article.classList.add("cella" , `${className}`);
         campoContainerEl.appendChild(article);
 
         article.addEventListener('click', function(){
@@ -33,4 +42,4 @@ playButton.addEventListener('click', function(){
             console.log(this.innerText);
         });
     }
-});
+}
